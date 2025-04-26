481d7c42db4d667f61178bac2b77c7ebd13429d3
indra <indradwiprabowo27@gmail.com>
2025-04-26 16:08:58 +0700

    perubahan

--- src/BreakAndContinue.java
+++ src/BreakAndContinue.java
@@ -1,12 +1,8 @@
 public class BreakAndContinue {
 	public static void main(String[] args) {
 
-		// break juga digunakan untuk menghentikan seluruh perulangan
-		// continue juga digunakan untuk menghentikan perulangan saat ini, lalu melanjutkannya keperulangan selanjutnya
-
 		var nilai = 1;
 
-		// kata kunci break
 		while (true) {
 			System.out.println("perulangan ke: " + nilai);
 			nilai++;
@@ -16,8 +12,6 @@ public class BreakAndContinue {
 			}
 		}
 		System.out.println("perulangan berhenti");
-
-		// kata kunci continue
 
 		for (int nilai1 = 1; nilai1 <= 100; nilai1++){
 			if (nilai1 % 2 == 0) {
